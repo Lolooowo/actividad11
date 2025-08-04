76aaf79b67944cfeaf77d706f03c2275efe5f2c2
Loloowo <julio.taracena132@gmail.com>
2025-08-04 11:21:44 -0600

    Se Cambia y de crea funcion de ordenamiento de carnet

--- actividad11.py
+++ actividad11.py
@@ -1,19 +1,106 @@
-def quickSort(lista):
+def OrdenarCarnet(lista):
     if len(lista) <= 1:
         return lista
-    pivote = lista[0]
-    mayores = [x for x in lista[1:] if x > pivote]
-    iguales = [x for x in lista if x == pivote]
-    menores = [x for x in lista[1:] if x < pivote]
-    return quickSort(menores) + iguales + quickSort(mayores)
-estudiantes = []
-num = int(input("Ingrese el numero de estudiantes que va a ingresar: "))
-for x in range(num):
-    nombre = input(f"Ingrese el nombre del estudiante No. {x + 1}: ")
-    estudiantes.append(nombre)
-ordenados = quickSort(estudiantes)
-i = 0
-for estudiante in ordenados:
-    print("Los estudiantes son: ")
-    print(f"No.{i+1} {estudiante}")
-    i+= 1
+    else:
+        pivote = lista["carnet"]
+        mayores = {x for x in lista["carnet"] if x > pivote}
+        menores = {x for x in lista["carnet"] if x < pivote}
+        return OrdenarCarnet(menores) + [pivote] + OrdenarCarnet(mayores)
+
+
+estudiantes ={}
+#cursos = {}
+def mostrarDatos(estudiante):
+    notaFinal = 0
+    notaTotal = 0
+    numCursos = 0
+    for carnet, datos in estudiante.items():
+        print(f"\nCarnet: {carnet}")
+        print(f"Nombre: {datos['nombre']}")
+        print(f"Edad: {datos['edad']}")
+        print(f"Carrera: {datos['carrera']}")
+        print("\tCursos: ")
+        for curso, nota in datos['nombreCurso'].items():
+            print(f"Curso: {curso}")
+            print(f"Nota de la tarea: {nota['notaTarea']}")
+            print(f"Nota del Parcial: {nota['notaParcial']}")
+            print(f"Nota del proyecto:{nota['notaProyecto']} ")
+            numCursos += 1
+            notaFinal = (nota['notaParcial'] + nota['notaProyecto']+ nota['notaTarea'])
+            notaTotal += notaFinal
+        print("\n Promedio final de todos los cursos: ")
+        notaFinal = notaFinal/numCursos
+        print(f"Nota final: {notaFinal}")
+
+
+
+
+while True:
+    print("1. Registrar estudiantes")
+    print("2. Mostrar todos los estudiantes y sus cursos")
+    print("3. Buscar estudiante por carnet")
+    print("4. SALIR")
+    opcion = int(input("Selecciona una opcion: "))
+    match opcion:
+        case 1:
+            cantidad = int(input("Cuantos estudiantes desea ingresar?: "))
+            for i in range(cantidad):
+                carnet = input("Ingresa carnet del estudiante: ")
+                nombre = input("Ingresa nombre completo del estudiante: ")
+                edad = int(input("Ingresa edad del estudiante: "))
+                carrera = input("Ingresa carrera del estudiante: ")
+                estudiantes[carnet] = {
+                    "nombre": nombre,
+                    "edad": edad,
+                    "carrera": carrera,
+                }
+                cantidadCursos = int(input("Cuantos cursos desea ingresar?: "))
+                if cantidadCursos > 0:
+                    for x in range(cantidadCursos):
+                        nombreCurso = input("Ingresa nombre del curso: ")
+                        notaTarea = int(input("Ingresa nota de la tarea del curso: "))
+                        if notaTarea<=60 and notaTarea>=0:
+                            notaParcial = int(input("Ingresa nota del parcial del curso: "))
+                            if notaParcial<=20 and notaParcial>=0:
+                                notaProyecto = int(input("Ingresa nota del proyecto del curso: "))
+                                if notaProyecto<=20 and notaProyecto>=0:
+                                    estudiantes[nombreCurso] = {
+                                    "notaTarea": notaTarea,
+                                    "notaParcial": notaParcial,
+                                    "notaProyecto": notaProyecto,
+                                    }
+                                else:
+                                    print("Ingrese una nota correcta.")
+                            else:
+                                print("Ingrese una nota correcta.")
+                        else:
+                            print("Ingrese una nota correcta.")
+
+
+                else:
+                    print("Ingrese una cantidad correcta.")
+
+        case 2:
+            print("Todos los estudiantes registrados.")
+            print("Estudiantes ordenados por su Carnet")
+            estudiantesOrdenadosCarnet = quickSort(estudiantes)
+            print("Estudiantes ordenados por su Nombre: ")
+            estudiantesOrdenadosNombre =
+        case 3:
+            buscar = input("Ingrese el carnet del estudiante: ")
+            if buscar in estudiantes:
+                estudiante = estudiantes[buscar]
+                print(f"Nombre: {estudiante['nombre']}")
+                print(f"Edad: {estudiante['edad']}")
+                print(f"Carrera: {estudiante['carrera']}")
+                print("\tCursos: ")
+                for curso, nota in estudiante['cursos'].items():
+                    print(f"Curso: {curso}")
+                    print(f"Nota de la tarea: {nota['notaTarea']}")
+                    print(f"Nota del Parcial: {nota['notaParcial']}")
+                    print(f"Nota del proyecto: {nota['notaProyecto']}")
+            else:
+                print("Estudiante no encontrado.")
+        case 4:
+            print("Saliendo...")
+            break
